de43b4b7e60dd92039ab7f8337b93d561f8e9b96
ful16022 <131100218+ful16022@users.noreply.github.com>
2023-04-21 21:38:02 -0500

    added last mod date

--- scripts/currentdate.js
+++ scripts/currentdate.js
@@ -1,4 +1,17 @@
-      // toLocaleDateString
-      const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-      document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
-      
+const date = new Date(Date.UTC(2012, 11, 20, 3, 0, 0, 200));
+const options = {
+  year: "numeric",
+  month: "numeric",
+  day: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+  second: "numeric",
+  hour12: false,
+};
+
+// this sets the year for the copyright
+document.querySelector("#currentyear").textContent = new Date().getFullYear();
+
+// this sets the file last modified date
+let myDate = new Date(document.lastModified);
+document.querySelector("#lastmod").textContent = new Intl.DateTimeFormat("en-US", options).format(date);
