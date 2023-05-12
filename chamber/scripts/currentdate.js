91715179317da5e63a4e30b5899e3a1288fa2e93
ful16022 <131100218+ful16022@users.noreply.github.com>
2023-05-11 21:48:38 -0500

    fixed mod date

--- chamber/scripts/currentdate.js
+++ chamber/scripts/currentdate.js
@@ -1,4 +1,10 @@
-      // toLocaleDateString
-      const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-      document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
-      
+// get the date 
+const date = new Date(Date.UTC(2012, 11, 20, 3, 0, 0, 200));
+
+// this sets the year for the copyright
+document.querySelector("#currentyear").textContent = new Date().getFullYear();
+
+// sets the last modified date on the page
+document.querySelector("#lastmod").textContent = document.lastModified;
+
+
--- scripts/currentdate.js
+++ scripts/currentdate.js
@@ -1,17 +1,9 @@
+// get the date 
 const date = new Date(Date.UTC(2012, 11, 20, 3, 0, 0, 200));
-const options = {
-  year: "numeric",
-  month: "numeric",
-  day: "numeric",
-  hour: "numeric",
-  minute: "numeric",
-  second: "numeric",
-  hour12: false,
-};
 
 // this sets the year for the copyright
 document.querySelector("#currentyear").textContent = new Date().getFullYear();
 
-// this sets the file last modified date
-let myDate = new Date(document.lastModified);
-document.querySelector("#lastmod").textContent = new Intl.DateTimeFormat("en-US", options).format(date);
+// sets the last modified date on the page
+document.querySelector("#lastmod").textContent = document.lastModified;
+
