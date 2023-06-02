20b86369982b116f51e611a49785597e80d04b37
ful16022 <131100218+ful16022@users.noreply.github.com>
2023-06-01 20:01:59 -0500

    activity progressive load

---
+++ week07/scripts/lazy.js
@@ -0,0 +1,33 @@
+const myImages = document.querySelectorAll("img[data-src]")
+
+const lazyLoad = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"))
+    image.onload = () => {
+        image.removeAttribute("data-src")
+    };
+};
+
+
+const options = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+}
+
+if ('IntersectionObserver' in window) {
+    const obsrvr = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if(item.isIntersecting) {
+                lazyLoad(item.target)
+                observer.unobserve(item.target)
+            }
+        })
+    }, options)
+    myImages.forEach((img) => {
+        obsrvr.observe(img)
+    })
+}
+else {
+    myImages.forEach((img) => {
+        lazyLoad(img)
+    })
+}
