7bf836ef749e30958ff6a6c0140ce94efcfa2853
ful16022 <131100218+ful16022@users.noreply.github.com>
2023-06-03 13:41:57 -0500

    update local storage

--- chamber/scripts/localstorage.js
+++ chamber/scripts/localstorage.js
@@ -1,21 +1,36 @@
+
 // initialize display elements
-const todayDisplay = document.querySelector(".today");
+const daysLastVisit = document.querySelector(".dayslastvisit");
 const visitsDisplay = document.querySelector(".visits");
+const todayDate = Date.now();
+const msToDays = 86400000;
+//const msToDays = 60000;
 
 
 // get the stored value in localStorage
 let numVisits = Number(window.localStorage.getItem("visits-ls"));
+let lastVisit = Number(window.localStorage.getItem("lastvisit-ls")) || 0;
 
 // determine if this is the first visit or display the number of visits.
 if (numVisits !== 0) {
 	visitsDisplay.textContent = numVisits;
+	let daysFromLast = (todayDate - lastVisit) / msToDays;
+	if (daysFromLast < 1) {
+		// if last visit is the same day display 0
+		daysLastVisit.textContent = 0 + " Days";
+	} else {
+		// if the last visit is 1 or more days display the number of days
+		daysLastVisit.textContent = Math.ceil(daysFromLast) + " Days";
+	}
 } else {
+	// if this is the first visit display first visit
 	visitsDisplay.textContent = `This is your first visit!`;
+	daysLastVisit.textContent = `This is your first visit!`;
 }
 
 // increment the number of visits.
 numVisits++;
-// store the new number of visits value
+// store the new number of visits
 localStorage.setItem("visits-ls", numVisits);
-// show todays date.
-todayDisplay.textContent = (Date.now()/1000/60/60/24).toFixed(0);
+// store the date of today as the last visit day
+localStorage.setItem("lastvisit-ls", todayDate);
