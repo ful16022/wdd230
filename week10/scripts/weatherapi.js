3acf9372bfa465153c6a5a3cbaafc454980ea68f
ful16022 <131100218+ful16022@users.noreply.github.com>
2023-06-20 22:43:53 -0500

    weather page

---
+++ week10/scripts/weatherapi.js
@@ -0,0 +1,46 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = "http://api.openweathermap.org/data/2.5/weather?q=Friendswood&units=imperial&appid=39b872f8e87273fe71de9bf6dc5a8b5f"
+
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // this is for testing the call
+      displayResults(data);
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+function displayResults(weatherData) {
+  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+  const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`;
+  const desc = weatherData.weather[0].description;
+
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = capitalize(desc);
+}
+
+function capitalize(s) {
+  const words = s.split(" ");
+  console.log(words);
+
+  // loop array to change the first letter in each array itme to cap
+  for (let i = 0; i < words.length; i++){
+    words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+  }
+
+  return words.join(" ");
+}
+
+apiFetch();
