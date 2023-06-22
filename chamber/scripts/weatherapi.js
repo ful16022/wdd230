3049f2c3d1e7427b67782ff9d5f582194a90a9c1
ful16022 <131100218+ful16022@users.noreply.github.com>
2023-06-21 20:03:44 -0500

    weather

--- chamber/scripts/weatherapi.js
+++ chamber/scripts/weatherapi.js
@@ -2,6 +2,7 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
+const windspeed =document.querySelector('#windspeed');
 
 const url = "https://api.openweathermap.org/data/2.5/weather?q=Friendswood&units=imperial&appid=39b872f8e87273fe71de9bf6dc5a8b5f"
 
@@ -11,7 +12,7 @@ async function apiFetch() {
     const response = await fetch(url);
     if (response.ok) {
       const data = await response.json();
-      console.log(data); // this is for testing the call
+      //console.log(data); // this is for testing the call
       displayResults(data);
     } else {
         throw Error(await response.text());
@@ -23,12 +24,28 @@ async function apiFetch() {
 
 function displayResults(weatherData) {
   currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}&deg;F</strong>`;
-  const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`;
+  const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
   const desc = weatherData.weather[0].description;
 
   weatherIcon.setAttribute('src', iconsrc);
   weatherIcon.setAttribute('alt', desc);
   captionDesc.textContent = capitalize(desc);
+  windspeed.textContent = weatherData.wind.speed.toFixed(1);
+
+  const temp = weatherData.main.temp;
+  const wind = weatherData.wind.speed;
+
+  
+
+  // if temp is below 50 and wind is higher than 3 do this
+if (temp <= 50 && wind > 3) 
+{
+  // calc the windchill
+  let windchill = 35.74 + 0.6215 * temp -35.75 * wind **0.16 + 0.4275 * temp * wind ** 0.16;
+  // display the windchill on the page
+  document.querySelector('#windchill').textContent = windchill.toFixed(1);
+
+}
 }
 
 function capitalize(s) {
