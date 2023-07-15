ab15492e53619ecfb3b2e45f663fccd55bcc865c
ful16022 <131100218+ful16022@users.noreply.github.com>
2023-07-15 18:29:00 -0500

    header done

--- bountiful/scripts/menu.js
+++ bountiful/scripts/menu.js
@@ -5,7 +5,7 @@ btn.addEventListener("click", toggleMenu);
 
 
 function toggleMenu() {
-  document.getElementById("primaryNav").classList.toggle("open");
+  document.getElementById("primarynav").classList.toggle("open");
   document.getElementById("hamburgerBtn").classList.toggle("open");
 }
 
